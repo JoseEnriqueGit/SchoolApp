0c4e2006ad09c6f45dc375bac83b5e8c48df1da1
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-05-21 18:36:34 -0400

    MatInputModule,     MatIconModule,     FormsModule,     MatTableModule,     MatButtonModule imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,26 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './Pages/components/nav/nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
-import { StudentViewComponent } from './Pages/student-view/student-view.component';
+import { StudentViewComponent } from './Pages/student-view/StudentView.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { AddStudentComponent } from './Pages/student-view/components/add-student/AddStudent.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, StudentViewComponent],
+  declarations: [AppComponent, NavComponent, StudentViewComponent, AddStudentComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatTabsModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    MatTableModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
