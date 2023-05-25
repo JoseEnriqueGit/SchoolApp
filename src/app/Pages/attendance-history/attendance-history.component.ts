1b09068731cc33d2ee4b7a721b91144b2a0a115b
Enrique.Git <75050597+JoseEnriqueGit@users.noreply.github.com>
2023-05-24 21:19:10 -0400

    HistoryViewComponent: Done

---
+++ src/app/Pages/attendance-history/attendance-history.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface Asistencia {
+  id_estudiante: string;
+  fecha: Date;
+  presente: boolean;
+}
+
+@Component({
+  selector: 'HistoryViewComponent',
+  templateUrl: './attendance-history.component.html',
+  styleUrls: ['./attendance-history.component.scss'],
+})
+export class AttendanceHistoryComponent {
+  asistencias: { [fecha: string]: Asistencia[] } = {};
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Asistencia[]>('https://schoolapi-vkp2.onrender.com/all-asistencia')
+      .subscribe(data => {
+        data.forEach(asistencia => {
+          const fecha = new Date(asistencia.fecha).toISOString().split('T')[0];
+          if (!this.asistencias[fecha]) {
+            this.asistencias[fecha] = [];
+          }
+          this.asistencias[fecha].push(asistencia);
+        });
+      });
+  }
+}
+
